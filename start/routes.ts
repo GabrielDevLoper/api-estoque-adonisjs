aedfd0927a5dbeeb7004fb76fe4e6b7d0ab43bcf
Gabriel Barreto <gabriel.devloper@gmail.com>
2021-08-30 12:08:07 -0300

    fix: ajustantdo autenticação nas rotas.

--- start/routes.ts
+++ start/routes.ts
@@ -1,30 +1,12 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
+
+Route.post('/usuario/cadastrar', 'UsuariosController.store')
 
 Route.post('/login', 'AuthController.login')
 
 Route.group(() => {
-    Route.resource('/usuarios', 'UsuariosController').apiOnly()
-    Route.resource('/perfis', 'PerfisController').apiOnly()
-    Route.resource('/categorias', 'CategoriasController').apiOnly()
-    Route.resource('/produtos', 'ProdutosController').apiOnly()
+  Route.resource('/usuarios', 'UsuariosController').apiOnly().except(['store'])
+  Route.resource('/perfis', 'PerfisController').apiOnly()
+  Route.resource('/categorias', 'CategoriasController').apiOnly()
+  Route.resource('/produtos', 'ProdutosController').apiOnly()
 }).middleware('auth')
